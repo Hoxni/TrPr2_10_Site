482ffdb2f7f1063fe03f95be63850b431d243ff4
Hoxni <anton.lugovoy.hopni@gmail.com>
2018-05-20 19:02:35 +0300

    Add files via upload

---
+++ 1/templates/main-text.php
@@ -0,0 +1,14 @@
+<?php
+
+$text_array = array(
+					'title' => 'Книжный магазин Booklogist.by',
+					'logo_name' => 'Booklogist.by',
+					'logo_text' => 'Покупка-продажа книг',
+					'nav_catalog' => 'Каталог',
+					'nav_order' => 'Заказать книгу',
+					'nav_buy' => 'Оформить покупку',
+					'nav_about' => 'О магазине',
+					'category_1' => 'Художественная литература',
+					'category_2' => 'Научная литература',
+					'category_3' => 'Детская литература',
+				);
